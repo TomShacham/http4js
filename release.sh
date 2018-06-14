7311fc4f410a7acb8937d54b4985266262f6cccb
Nate Higgins <nat@nath.is>
2018-06-14 15:45:13 +0100

    Move index file into src/main to fix build process, use local tsc to build

--- release.sh
+++ release.sh
@@ -3,10 +3,10 @@ set -x
 
 find src -type f -name "*.js" -delete && \
 find src -type f -name "*.d.ts" -delete && \
-tsc && \
+yarn build && \
 rm -rf dist && \
 cp -r src/main dist && \
-cp package.json tsconfig.json index.ts index.js dist && \
+cp package.json tsconfig.json dist && \
 pushd dist && \
 npm publish && \
 popd
