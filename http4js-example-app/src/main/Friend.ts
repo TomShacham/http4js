920766c02864e1bce19ab89b8c899bfb41754eab
tomshacham <shachamtom@gmail.com>
2018-08-27 09:23:09 +0100

    move out example app

--- http4js-example-app/src/main/Friend.ts
+++
@@ -1,8 +0,0 @@
-export class Friend {
-    name: string;
-
-    constructor(name: string) {
-        this.name = name;
-        return this;
-    }
-}
--- http4js-example-app/src/main/FriendDb.ts
+++
@@ -1,7 +0,0 @@
-import {Friend} from "./Friend";
-
-export interface FriendsDB {
-    all(): Promise<Array<Friend>>
-    add(friend: Friend): Promise<Friend>
-    deleteAll(): void
-}
