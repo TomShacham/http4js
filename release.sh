d3347ffb05f84e6a578f11d758d45970c4ff8710
TomShacham <shachamtom@gmail.com>
2018-11-07 07:49:10 +0000

    4.2.3

--- release.sh
+++ release.sh
@@ -1,17 +1,12 @@
 #!/usr/bin/env bash
 set -x
 
-read -p "Added release notes? " -r
-echo    # (optional) move to a new line
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
-    npm run build && \
-    npm run test && \
-    rm -rf dist && \
-    cp -r src/main dist && \
-    cp package.json tsconfig.json README.md dist && \
-    pushd dist && \
-    npm publish && \
-    popd && \
-    rm -r dist
-fi
+npm run build && \
+npm run test && \
+rm -rf dist && \
+cp -r src/main dist && \
+cp package.json tsconfig.json README.md dist && \
+pushd dist && \
+npm publish && \
+popd && \
+rm -r dist
